1218375ed5075ef9914d25b55ea28db19387f3bd
clinestanford <clinestanford@gmail.com>
2018-11-17 08:56:40 -0700

    finished hesitant logic for pairs trading

--- Cuzcobot/views.py
+++ Cuzcobot/views.py
@@ -21,24 +21,67 @@ def pairs(request):
 			#buy the security that is low, and sell the high
 			if pair.ticker1.currentPositionShares >= 1:
 				#sell ticker 1, buy ticker 2
+				if price1/avgPrice1 > price2/avgPrice2:
+					#sell ticker 1
+					#buy ticker 2
+					#pass
+
+				else:
+					#the percentage over price1 is lower than
+					#price 2, which means we should probably 
+					#hang onto it
+					pass
 
 			elif: pair.ticker2.currentPositionShares >= 1:
 				#sell ticker 2, buy ticker 1
+				if price2/avgPrice2 > price1/avgPrice1:
+					#sell ticker2
+					#buy ticker1
+					#pass
+
+				else:
+					#hang out for a bit, we own ticker2, 
+					#but the average price2 is lower than 
+					#average price1, so hang onto it
 
 			else:
 				#buy the "lower" of the two 
+
+				if price1/avgPrice1 > price2/avgPrice2:
+					#buy ticker2
+					#do nothing with ticker1
+				else:
+					#buy ticker1
+					#do nothing with ticker2
 
 
 		if currentSpread < averageSpread * pair.spreadLow:
 			#sell the lower and buy the higher
 			if pair.ticker1.currentPositionShares >= 1:
 				#sell ticker 1, buy ticker 2
+				if price1/avgPrice1 > price2/avgPrice2:
+					#sell ticker1
+					#buy ticker2
+					#pass
+				else:
+					#do nothing
 
 			elif: pair.ticker2.currentPositionShares >= 1:
 				#sell ticker 2, buy ticker 1
+				if price2/avgPrice2 > price1/avgPrice1:
+					#sell ticker2
+					#buy ticker1
+					#pass
+				else:
+					#do nothing
 
 			else:
 				# buy the "higher" of the two
+				if price1/avgPrice1 > price2/avgPrice2:
+					#buy ticker2
+
+				else:
+					#buy ticker1
 
 
 
