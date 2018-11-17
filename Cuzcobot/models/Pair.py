5aa1f7f9f006c395930056f00f66ca185861ca3f
JORDAN BIGELOW <jordan@jordanbigelow.com>
2018-11-16 20:05:40 -0700

    Added Database Support & Custom User Backend

--- Cuzcobot/models/Pair.py
+++ Cuzcobot/models/Pair.py
@@ -1,8 +1,10 @@
 from django.db import models
+from typing import Tuple
+from statsmodels.tsa.stattools import coint
 
 class Pair(models.Model):
-    ticker1 = models.CharField(max_length=8)
-    ticker2 = models.CharField(max_length=8)
+    ticker1 = models.ForeignKey('Cuzcobot.Security', on_delete=models.PROTECT)
+    ticker2 = models.ForeignKey('Cuzcobot.Security', on_delete=models.PROTECT, related_name='secondSecurity')
     window = models.DecimalField(max_digits=4,decimal_places=0)
 
     @property
