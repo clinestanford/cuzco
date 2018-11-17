78c89e4edc89d9ac1483c7b08e10bea1081e9350
clinestanford <clinestanford@gmail.com>
2018-11-16 22:12:09 -0700

    adding the logic of when to buy and sell

--- Cuzcobot/views.py
+++ Cuzcobot/views.py
@@ -17,11 +17,28 @@ def pairs(request):
 		averageSpread = max(avgPrice1, avgPrice2) - min(avgPrice1, avgPrice2)
 		currentSpread = max(price1, price2) - min(price1 - price2)
 
-		if currentSpread > averageSpread * pair.spreadHigh :
+		if currentSpread > averageSpread * pair.spreadHigh:
 			#buy the security that is low, and sell the high
+			if pair.ticker1.currentPositionShares >= 1:
+				#sell ticker 1, buy ticker 2
 
-		if currentSpread < averageSpread * pair.spreadLow :
+			elif: pair.ticker2.currentPositionShares >= 1:
+				#sell ticker 2, buy ticker 1
+
+			else:
+				#buy the "lower" of the two 
+
+
+		if currentSpread < averageSpread * pair.spreadLow:
 			#sell the lower and buy the higher
+			if pair.ticker1.currentPositionShares >= 1:
+				#sell ticker 1, buy ticker 2
+
+			elif: pair.ticker2.currentPositionShares >= 1:
+				#sell ticker 2, buy ticker 1
+
+			else:
+				# buy the "higher" of the two
 
 
 
