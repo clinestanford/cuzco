51509eb6e989d07458ea09cbc3f598678ce97831
JORDAN BIGELOW <jordan@jordanbigelow.com>
2018-11-16 22:07:38 -0700

    Addition to Transaction Model

--- Cuzcobot/models/Security.py
+++ Cuzcobot/models/Security.py
@@ -1,6 +1,7 @@
 from django.db import models
+from django.db.models import Sum
 
-from Cuzcobot.models import Price
+from Cuzcobot.models import Price, Transaction
 
 class Security(models.Model):
     tickerSymbol = models.CharField(max_length=6, verbose_name='Security Ticker Symbol')
@@ -10,3 +11,13 @@ class Security(models.Model):
     def price(self):
         a = Price.objects.filter(ticker=self).latest('priceDate')
         return a.current
+
+    @property
+    def currentPositionDollars(self):
+        return Transaction.objects.filter(ticker=self.tickerSymbol).aggregate(Sum('tradeValue'))['tradeValue__Sum']
+
+    @property
+    def currentPositionShares(self):
+        return Transaction.objects.filter(ticker=self.tickerSymbol).aggregate(Sum('shares'))['shares__Sum']
+
+
--- Cuzcobot/models/__init__.py
+++ Cuzcobot/models/__init__.py
@@ -7,3 +7,4 @@ from .ApplicationUser import ApplicationUser
 from .Pair import Pair
 from .Security import Security
 from .Price import Price
+from .Transaction import Transaction
