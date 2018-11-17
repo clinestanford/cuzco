424ee9aa7093e2e2524c97120957bb596e40cd89
clinestanford <clinestanford@gmail.com>
2018-11-16 19:41:30 -0700

    added test for cointegration in Pairs

--- Cuzcobot/models/Pair.py
+++ Cuzcobot/models/Pair.py
@@ -1,10 +1,34 @@
 from django.db import models
 
-class Pair(models.Model):
-    ticker1 = models.CharField(max_length=8)
-    ticker2 = models.CharField(max_length=8)
-    window = models.DecimalField(max_digits=4)
+import numpy as np
 
-    @property
-    def is_coIntegrated(self):
-        return True
+class Pair(models.Model):
+	ticker1 = models.CharField(max_length=8)
+	ticker2 = models.CharField(max_length=8)
+	window = models.DecimalField(max_digits=4)
+
+	#checks if the two pairs are still cointegrated for the last window frame
+	def check_cointegration(self, v1: list, v2: list) -> Tuple[bool, float]:
+		# Date indexed pricing data for stickers
+		p_value = coint(v1, v2)[1]
+		if p_value < 0.05:
+			return (True, p_value)
+		else: 
+			return (False, p_value)
+
+
+	def get_data(self, name):
+		#need to use the self.window value to return the right amount of data
+		
+
+		return 
+
+	@property
+	def is_cointegrated(self):
+		data1 = get_data(self.ticker1)
+		data2 = get_data(self.ticker2)
+
+		is_coint_bool, p_value = self.check_cointegration(data1, data2)
+
+		return is_coint_bool
+
